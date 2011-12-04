550d270446f0c9b66d8167ed7d2856efaaa0d98e
KIHARA Hideto <deton@m1.interq.or.jp>
2011-12-04 18:12:57 +0900

    Add script for uim-sh.

---
+++ uim-wordcount.scm
@@ -0,0 +1,15 @@
+(require "wordcount.scm")
+(define (read-input res)
+  (let ((ch (read-char)))
+    (if (eof-object? ch)
+      res
+      (read-input (cons ch res)))))
+
+(define str (list->string (reverse (read-input '()))))
+
+(display
+  (format "~a ~a ~a ~a\n"
+    (number->string (wordcount-count-line str))
+    (number->string (wordcount-count-word str))
+    (number->string (wordcount-count-char str))
+    (number->string (wordcount-count-byte str))))
