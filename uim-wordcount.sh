c7d6e21b332c969d6387179b7fefd91ae4116682
KIHARA Hideto <deton@m1.interq.or.jp>
2011-12-05 20:10:47 +0900

    Change not to use readlink for FreeBSD.

--- uim-wordcount.sh
+++ uim-wordcount.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
-scmfile=$(readlink -f $(dirname $0)/uim-wordcount.scm)
+dir=$(cd "$(dirname $0)"; pwd)
+# XXX: use realpath or readlink -f
+scmfile="$dir/uim-wordcount.scm"
 if [ ! -t 0 ]; then	# data from stdin
 	# XXX: bytes count may change after converting to utf-8
-	nkf -w | uim-sh $scmfile
+	nkf -w | uim-sh "$scmfile"
 else
 	for f
 	do
-		if [ -f $f ]; then
+		if [ -f "$f" ]; then
 			# XXX: bytes count may change after converting to utf-8
-			nkf -w $f | uim-sh $scmfile
+			nkf -w "$f" | uim-sh "$scmfile"
 		fi
 	done
 fi
