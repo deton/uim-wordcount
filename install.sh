8f365ec9e6ac6d5293c12af3b69edc18a5c2a66d
KIHARA Hideto <deton@m1.interq.or.jp>
2011-12-10 11:33:12 +0900

    Move icons to pixmaps/.

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 scmdir=$(pkg-config --variable=uim_scmdir uim)
 pixmapsdir=$(pkg-config --variable=uim_datadir uim)/pixmaps
 cp wordcount.scm "$scmdir"
-cp wordcount.png wordcount_dark_background.png "$pixmapsdir"
-cp wordcount.svg wordcount_dark_background.svg "$pixmapsdir"
+cp pixmaps/wordcount.png pixmaps/wordcount_dark_background.png "$pixmapsdir"
+cp pixmaps/wordcount.svg pixmaps/wordcount_dark_background.svg "$pixmapsdir"
 uim-module-manager --register wordcount
