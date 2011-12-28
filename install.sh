a79429bd77c53f782ed386ac9cdfcd1c3a5d078e
KIHARA Hideto <deton@m1.interq.or.jp>
2011-12-28 17:57:39 +0900

    Change to use $srcdir to install from another directory.

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+srcdir=$(dirname $0)
 scmdir=$(pkg-config --variable=uim_scmdir uim)
 pixmapsdir=$(pkg-config --variable=uim_datadir uim)/pixmaps
-cp wordcount.scm "$scmdir"
-cp pixmaps/wordcount.png pixmaps/wordcount_dark_background.png "$pixmapsdir"
-cp pixmaps/wordcount.svg pixmaps/wordcount_dark_background.svg "$pixmapsdir"
+cp "$srcdir/wordcount.scm" "$scmdir"
+cp "$srcdir/pixmaps/wordcount.png" "$srcdir/pixmaps/wordcount_dark_background.png" "$pixmapsdir"
+cp "$srcdir/pixmaps/wordcount.svg" "$srcdir/pixmaps/wordcount_dark_background.svg" "$pixmapsdir"
 uim-module-manager --register wordcount
